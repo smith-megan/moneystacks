244ba5f31dd1517488d6a105c40ec306b054dcd0
smith-megan <smith.megan.design@gmail.com>
2020-08-19 14:38:54 -0700

    Updates

---
+++ precache-manifest.23129d2330636711d802fcaf76f0f8d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83cf172cd2dfde3bac33c257d5bf7d9c",
+    "url": "/moneystacks/index.html"
+  },
+  {
+    "revision": "7b6454ead8a87568bd84",
+    "url": "/moneystacks/static/css/main.16971e0a.chunk.css"
+  },
+  {
+    "revision": "a3473f2e82a0d3698eb2",
+    "url": "/moneystacks/static/js/2.de4ff843.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moneystacks/static/js/2.de4ff843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b6454ead8a87568bd84",
+    "url": "/moneystacks/static/js/main.314dacbe.chunk.js"
+  },
+  {
+    "revision": "190dae8664cd0875b016",
+    "url": "/moneystacks/static/js/runtime-main.c8e6396d.js"
+  },
+  {
+    "revision": "b05a4b37ba6be21f8e171c2f66236bd4",
+    "url": "/moneystacks/static/media/banner.b05a4b37.png"
+  },
+  {
+    "revision": "3297cfd4ecb05f1b4c15e3d524410392",
+    "url": "/moneystacks/static/media/moneystack.3297cfd4.jpg"
+  }
+]);
